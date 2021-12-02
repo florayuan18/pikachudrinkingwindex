c69928821c5038fd73c0b65146f51e45cda79879
4DISEASE <4disease@gmail.com>
2021-12-02 10:24:44 -0800

    SCSS integrations

--- src/main/java/com/example/sping_portfolio/controllers/WebAPIController.java
+++ src/main/java/com/example/sping_portfolio/controllers/WebAPIController.java
@@ -22,41 +22,44 @@ import java.net.http.HttpResponse;
 @Controller
 public class WebAPIController {
 
-    // GET request, no parameters
-    @GetMapping("/covid19")
-    public String covid19(Model model) throws IOException, InterruptedException, ParseException {
-//         https://rapidapi.com/spamakashrajtech/api/corona-virus-world-and-india-data/
-//        rapidapi setup:
+//    // GET request, no parameters
+//    @GetMapping("/covid19")
+//    public String covid19(Model model) throws IOException, InterruptedException, ParseException {
+////         https://rapidapi.com/spamakashrajtech/api/corona-virus-world-and-india-data/
+////        rapidapi setup:
+////        HttpRequest request = HttpRequest.newBuilder()
+////                .uri(URI.create("https://corona-virus-world-and-india-data.p.rapidapi.com/api"))
+////                .header("x-rapidapi-key", "dec069b877msh0d9d0827664078cp1a18fajsn2afac35ae063")
+////                .header("x-rapidapi-host", "corona-virus-world-and-india-data.p.rapidapi.com")
+////                .method("GET", HttpRequest.BodyPublishers.noBody())
+////                .build();
+////
+////        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
 //        HttpRequest request = HttpRequest.newBuilder()
-//                .uri(URI.create("https://corona-virus-world-and-india-data.p.rapidapi.com/api"))
-//                .header("x-rapidapi-key", "dec069b877msh0d9d0827664078cp1a18fajsn2afac35ae063")
-//                .header("x-rapidapi-host", "corona-virus-world-and-india-data.p.rapidapi.com")
+//                .uri(URI.create("https://rawg-video-games-database.p.rapidapi.com/games/%7Bgame_pk%7D"))
+//                .header("x-rapidapi-host", "rawg-video-games-database.p.rapidapi.com")
+//                .header("x-rapidapi-key", "e2ec6cd693msh31f1d4d692bd6b0p1dcb11jsn9ee520fd4398")
 //                .method("GET", HttpRequest.BodyPublishers.noBody())
 //                .build();
-//
 //        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://rawg-video-games-database.p.rapidapi.com/games/%7Bgame_pk%7D"))
-                .header("x-rapidapi-host", "rawg-video-games-database.p.rapidapi.com")
-                .header("x-rapidapi-key", "e2ec6cd693msh31f1d4d692bd6b0p1dcb11jsn9ee520fd4398")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        //alternative #1: convert response.body() to java hash map
-        var map = new ObjectMapper().readValue(response.body(), HashMap.class);
+//        //alternative #1: convert response.body() to java hash map
+//        var map = new ObjectMapper().readValue(response.body(), HashMap.class);
+//
+////        //alternative #2: convert response.body() to JSON object
+////        Object obj = new JSONParser().parse(response.body());
+////        JSONObject jo = (JSONObject) obj;
+////
+////        //pass stats to view
+////        model.addAttribute("map", map);
+////        model.addAttribute("jo", jo);
+////        model.addAttribute("world_map", map.get("world_total")); //illustrative of map get
+////        model.addAttribute("world_jo", jo.get("world_total"));  //illustrative of jo get
+////
+////
+////        return "covid19";
+////    }
+//
+////}
 
-        //alternative #2: convert response.body() to JSON object
-        Object obj = new JSONParser().parse(response.body());
-        JSONObject jo = (JSONObject) obj;
-
-        //pass stats to view
-        model.addAttribute("map", map);
-        model.addAttribute("jo", jo);
-        model.addAttribute("world_map", map.get("world_total")); //illustrative of map get
-        model.addAttribute("world_jo", jo.get("world_total"));  //illustrative of jo get
-
-
-        return "covid19";
-    }
-
+    
 }
