078e9da228851f6697b0067495bcad6b4b44ddea
florayuan18 <flora.yuan18@gmail.com>
2021-12-10 09:13:06 -0800

    about us page updated

---
+++ src/main/java/com/example/sping_portfolio/controllers/AboutFloraCTRL.java
@@ -0,0 +1,48 @@
+package com.example.sping_portfolio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AboutFloraCTRL {
+    @GetMapping("/AboutUs/aboutflora")
+    public String TableCTRL(@RequestParam(name="seq", required=false, defaultValue= "") String seq,
+                            @RequestParam(name="changeSeq", required=false, defaultValue= "") String changeSeq,
+                            @RequestParam(name="insertSeq", required=false, defaultValue="") String insertSeq,
+                            @RequestParam(name="insertSeqLoc", required=false, defaultValue="0") int insertSeqLoc,
+                            @RequestParam(name="removeSeq", required=false, defaultValue="") String removeSeq,
+                            @RequestParam(name="xDist", required=false, defaultValue="0") int xDist,
+                            @RequestParam(name="yDist", required=false, defaultValue="0") int yDist,
+                            Model model)
+    {
+
+        String binaryC = "";
+
+        LightSequence gradShow = new LightSequence(seq);
+        String binary = gradShow.displayRet();
+
+        if (changeSeq.equals("")) {
+            binaryC = binary;
+        }
+        else{
+            binaryC = gradShow.changeSequence(changeSeq);
+        }
+        String binaryI = gradShow.insertSegment(insertSeq, insertSeqLoc);
+        String binaryR = gradShow.removeSegment(removeSeq);
+        Double distXY = gradShow.distCalc(xDist,yDist);
+
+
+        model.addAttribute("binary", binary);
+        model.addAttribute("binaryC", binaryC);
+        model.addAttribute("binaryI", binaryI);
+        model.addAttribute("binaryR", binaryR);
+        model.addAttribute("distXY", distXY);
+
+        return "/AboutUs/aboutflora";
+    }
+}
+
+
+
