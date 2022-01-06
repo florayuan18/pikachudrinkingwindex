e4d0a6a7bc36d070d5281c5f04dc5f35bd072f97
4DISEASE <4disease@gmail.com>
2022-01-06 10:05:55 -0800

    DESIGN

--- src/main/java/com/example/sping_portfolio/PersonSqlMvcController.java
+++ src/main/java/com/example/sping_portfolio/PersonSqlMvcController.java
@@ -110,7 +110,7 @@ public class PersonSqlMvcController implements WebMvcConfigurer {
         model.addAttribute("returnString", returnString);
 
 
-        return "AboutUs/aboutkira";
+        return "aboutkira";
     }
 
 
