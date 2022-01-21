5b648df6767804486ed5ffff6b405e16ae68548b
florayuan18 <flora.yuan18@gmail.com>
2022-01-21 01:11:54 -0800

    frq5 part 2

--- src/main/java/com/example/sping_portfolio/controllers/FloraFRQ/FloraPasswordGenerator.java
+++ src/main/java/com/example/sping_portfolio/controllers/FloraFRQ/FloraPasswordGenerator.java
@@ -2,19 +2,20 @@ package com.example.sping_portfolio.controllers.FloraFRQ;
 import java.util.Random;
 
 public class FloraPasswordGenerator {
-    private static double generatedPasswords=0;
+    private static int generatedPasswords=0;
     private static Random random = new Random();
     private String prefix;
-    private double length
+    private int length;
 
-    public PasswordGenerator(String prefix, double length){
+    public FloraPasswordGenerator(String prefix, int length){
         this.prefix="A";
         this.length=length;
     }
     public String pwGen(){
         String pwd = this.prefix+".";
-        for(int i=1, i<=this.length;i++){
-            pwd+=random.nextInt(10);        }
+        for(int i=1; i<=this.length;i++) {
+            pwd += random.nextInt(10);
+        }
         generatedPasswords+=1;
         return pwd;
     }
