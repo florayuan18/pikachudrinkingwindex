f8ebf530a9bafbb861df859aa43f3e3209e41b01
florayuan18 <flora.yuan18@gmail.com>
2022-01-21 00:44:58 -0800

    frq5 part 1

--- src/main/java/com/example/sping_portfolio/controllers/FloraFRQ/FloraInvitation.java
+++ src/main/java/com/example/sping_portfolio/controllers/FloraFRQ/FloraInvitation.java
@@ -3,22 +3,29 @@ package com.example.sping_portfolio.controllers.FloraFRQ;
 public class FloraInvitation {
     private String hostName = "Karen";
     private String address;
-    public String getHostName(){
+
+    public String getHostName() {
         return hostName;
     }
-    public String setAddress(String ad){
-        address=ad;
+
+    public String setAddress(String ad) {
+        address = ad;
         return address;
     }
-    public FloraInvitation(String n, String a){
+
+    public FloraInvitation(String n, String a) {
         hostName = n;
         address = a;
     }
-    public String message(String guestName){
+
+    public String message(String guestName) {
         return "Dear " + guestName + ", please attend my event at " + address + ". See you then, " + hostName;
     }
-
+/*
     public static void main(String[] args) {
-        FloraInvitation Guest= new FloraInvitation();
-    }
+        FloraInvitation party = new FloraInvitation("Wilma", "1234567 Eleven Lane");
+        party.displayhostName();
+        party.updateAddress("1234 Walnut Street");
+        party.message("Cheryl");
+    } */
 }
