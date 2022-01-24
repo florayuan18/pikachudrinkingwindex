981b294bf395b25dc160d240ee977b9d2efc68d9
crystalwidjaja <pansypig@gmail.com>
2022-01-24 10:20:09 -0800

    frq 8 fix

--- src/main/java/com/example/sping_portfolio/controllers/CrystalFRQ/CrystalPlotMain.java
+++ src/main/java/com/example/sping_portfolio/controllers/CrystalFRQ/CrystalPlotMain.java
@@ -4,27 +4,27 @@ public class CrystalPlotMain {
     public static void main(String[] args)
 
     {
-        Plot p1 = new Plot("corn",20);
-        Plot p2 = new Plot("corn",30);
-        Plot p3 = new Plot("peas",10);
-        Plot p4 = new Plot("peas",30);
-        Plot p5 = new Plot("corn",40);
-        Plot p6 = new Plot("corn",62);
-        Plot p7 = new Plot("wheat",10);
-        Plot p8 = new Plot("corn",50);
-        Plot p9 = new Plot("rice",30);
-        Plot p10 = new Plot("corn",55);
-        Plot p11 = new Plot("corn",30);
-        Plot p12 = new Plot("peas",30);
-        Plot[][] plots = {{p1,p2,p3},
+        CrystalPlot p1 = new CrystalPlot("corn",20);
+        CrystalPlot p2 = new CrystalPlot("corn",30);
+        CrystalPlot p3 = new CrystalPlot("peas",10);
+        CrystalPlot p4 = new CrystalPlot("peas",30);
+        CrystalPlot p5 = new CrystalPlot("corn",40);
+        CrystalPlot p6 = new CrystalPlot("corn",62);
+        CrystalPlot p7 = new CrystalPlot("wheat",10);
+        CrystalPlot p8 = new CrystalPlot("corn",50);
+        CrystalPlot p9 = new CrystalPlot("rice",30);
+        CrystalPlot p10 = new CrystalPlot("corn",55);
+        CrystalPlot p11 = new CrystalPlot("corn",30);
+        CrystalPlot p12 = new CrystalPlot("peas",30);
+        CrystalPlot[][] plots = {{p1,p2,p3},
                 {p4,p5,p6},
                 {p7,p8,p9},
                 {p10,p11,p12}};
 
-        ExperimentalFarm f = new ExperimentalFarm(plots);
-        Plot highestYield = f.getHighestYield("corn");
-        Plot highestYield1 = f.getHighestYield("peas");
-        Plot highestYield2 = f.getHighestYield("bananas");
+        CrystalExperimentalFarm f = new CrystalExperimentalFarm(plots);
+        CrystalPlot highestYield = f.getHighestYield("corn");
+        CrystalPlot highestYield1 = f.getHighestYield("peas");
+        CrystalPlot highestYield2 = f.getHighestYield("bananas");
 
         try {
             System.out.println(highestYield.toString());
