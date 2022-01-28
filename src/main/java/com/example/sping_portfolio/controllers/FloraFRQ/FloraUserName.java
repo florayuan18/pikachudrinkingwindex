b32166bfbd6bfa10f82a905d570c2f8bf2d3c5e7
florayuan18 <flora.yuan18@gmail.com>
2022-01-27 23:45:36 -0800

    frq 7

---
+++ src/main/java/com/example/sping_portfolio/controllers/FloraFRQ/FloraUserName.java
@@ -0,0 +1,36 @@
+package com.example.sping_portfolio.controllers.FloraFRQ;
+import com.example.sping_portfolio.controllers.FloraFRQ.FloraUserName;
+
+import java.util.ArrayList;
+
+public class FloraUserName
+{
+    private ArrayList<String> possibleNames;
+
+    public FloraUserName(String firstName, String lastName)
+    {
+        for (int i = 0; i < firstName.length(); i++)
+        {
+
+        }
+
+    }
+
+
+
+    /** Returns true if arr contains name, and false otherwise. */
+
+    public boolean isUsed(String name, String[] arr)
+
+    { /* implementation not shown */ }
+
+
+
+    /** Removes strings from possibleNames that are found in usedNames as described in part (b).
+
+     */
+
+    public void setAvailableUserNames(String[] usedNames)
+
+    { /* to be implemented in part (b) */ }
+}
