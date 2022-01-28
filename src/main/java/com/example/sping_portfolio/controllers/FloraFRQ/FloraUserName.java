8389e6e41294710920c85966fad96cfb0a393aaf
florayuan18 <flora.yuan18@gmail.com>
2022-01-28 00:58:51 -0800

    frq 7

--- src/main/java/com/example/sping_portfolio/controllers/FloraFRQ/FloraUserName.java
+++ src/main/java/com/example/sping_portfolio/controllers/FloraFRQ/FloraUserName.java
@@ -48,25 +48,36 @@ public class FloraUserName
 
     public void setAvailableUserNames(String[] usedNames)
     {
-        for (int i = (possibleNames.size() - 1); i++)
+        System.out.printf("+++ size=%d",possibleNames.size());
+        for (int i = (possibleNames.size() - 1); i >= 0; i--)
         {
+
             String pn = possibleNames.get(i);
 
             if (isUsed(pn, usedNames))
             {
                 possibleNames.remove(i);
             }
-
-
         }
     }
 
+    public void printTest()
+    {
+        System.out.printf("=========================\n");
+        for (int n = 0; n < possibleNames.size(); n++)
+        {
+            System.out.printf("n=%d, name = %s\n",n, possibleNames.get(n));
+        }
+    }
 
     public static void main(String[] args) {
         FloraUserName florausername = new FloraUserName("John", "Smith");
         String[] usedNames = {"harta", "hartm", "harty"};
+        florausername.setAvailableUserNames(usedNames);
+        florausername.printTest();
 
-        if(florausername.isUsed("harta", usedNames))
+        // testing
+       /* if(florausername.isUsed("harta", usedNames))
         {
             System.out.printf("True");
         }
@@ -74,6 +85,8 @@ public class FloraUserName
         {
             System.out.printf("False");
         }
+        */
+
 
     }
 }
