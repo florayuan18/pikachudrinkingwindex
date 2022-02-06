0bf5e359bd9efff166cf2a45c10084de635a4d0b
crystalwidjaja <pansypig@gmail.com>
2022-02-05 17:02:55 -0800

    frq 7

--- src/main/java/com/example/sping_portfolio/controllers/CrystalFRQ/CrystalUser.java
+++ src/main/java/com/example/sping_portfolio/controllers/CrystalFRQ/CrystalUser.java
@@ -3,34 +3,51 @@ package com.example.sping_portfolio.controllers.CrystalFRQ;
 
 import java.util.ArrayList;
 
-public class CrystalUser {
-    private String firstName;
-    private String lastName;
-    private ArrayList<String> possibleNames;
 
-        public CrystalUser(String firstName, String lastName) {
-            possibleNames = new ArrayList<String>();
-            for (int i = 0; i < firstName.length(); i++) {
-                possibleNames.add (lastName + firstName.substring(0, i + 1 ));
-            }
+public class CrystalUser {
+
+// The list of possible user names, based on a user’s first and last names and initialized by the constructor.
+    private ArrayList<String> possibleNames = new ArrayList<>(){};
+
+    public CrystalUser(String firstName, String lastName){
+        for(int i = 1; i < firstName.length(); i++){
+            String subFirst = firstName.substring(0, i);
+            possibleNames.add(lastName + subFirst);
         }
 
+        }
 
-      //  public boolean isUsed(String name, String[] arr)
+    public boolean isUsed(String name, String[] arr){
+        for (String s : arr){
+            if (s.equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-
-
-/*
-        public void setAvailableUserNames(String[] usedNames) {
-            for (int i = possibleNames.size() - 1; i >= 0; i--) {
-                if (isUsed(possibleNames.get(i), usedNames))
-                {
+    public void setAvailableUserNames(String[] usedNames){
+        ArrayList<String> pN = possibleNames;
+        System.out.println(pN);
+        for (String nn : usedNames) {
+            for (int i = 0; i < pN.size(); i++) {
+                if (nn.equals(pN.get(i))) {
                     possibleNames.remove(i);
+//                    System.out.println(pN.get(i));
                 }
             }
         }
-*/
+        for (String n : possibleNames){
+            System.out.println(n);
+        }
 
 
 
     }
+
+    public static void main(String [] args) {
+        CrystalUser person = new CrystalUser("john", "smith");
+        String[] used = {"smith", "smithj", "harty"};
+        person.setAvailableUserNames(used);
+    }
+}
