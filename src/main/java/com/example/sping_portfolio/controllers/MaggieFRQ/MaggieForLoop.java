76d0f9c7f975091a2a5392fc0ae21c43f20aee71
maggie3000 <maggiekillada@gmail.com>
2022-02-06 12:27:17 -0800

    frq6

--- src/main/java/com/example/sping_portfolio/controllers/MaggieFRQ/MaggieForLoop.java
+++ src/main/java/com/example/sping_portfolio/controllers/MaggieFRQ/MaggieForLoop.java
@@ -1,11 +1,17 @@
+/*
+Maggie Killada
+AP CSA FRQ #6
+ */
+
 package com.example.sping_portfolio.controllers.MaggieFRQ;
 
 public class MaggieForLoop {
-    public static void main(String[] args){
-        String words[] = {"ten","fading","post","card","thunder","hinge","trailing","batting"};
-        for (int i = 0; i < words.length; i++){
-
+    public static void main(String[] args) {
+        String words[] = {"ten", "fading", "post", "card", "thunder", "hinge", "trailing", "batting"};
+        for (String i : words) {
+            if (i.endsWith("ing")) {
+                System.out.println(i);
+            }
         }
-
     }
 }
