2f2a429ac8c229f961f8e50cf0adfab9702f6f2a
maggie3000 <maggiekillada@gmail.com>
2022-02-14 10:37:14 -0800

    controller code

--- src/main/java/com/example/sping_portfolio/controllers/Recommendations.java
+++ src/main/java/com/example/sping_portfolio/controllers/Recommendations.java
@@ -17,6 +17,7 @@ import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.Comparator;
 
+/*
 class StrategyComparator implements Comparator<String[]> {
     public int compare(String[] a, String[] b) {
         if (Float.parseFloat(a[4]) < Float.parseFloat(b[4])) {
@@ -38,19 +39,19 @@ class HelloComparator implements Comparator<String[]> {
         return 0;
     }
 }
+ */
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
-public class Reccomendations {
+public class Recommendations {
     @GetMapping("/recommendations")
     // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String RawgAPI(@RequestParam(name="search", required=true, defaultValue= "") String search,
-                          @RequestParam(name="rpgSearch", required=false, defaultValue= "false") boolean rpgSort,
+    public String RawgAPI(@RequestParam(name="rpgSearch", required=false, defaultValue= "false") boolean rpgSort,
                           @RequestParam(name="horrorSort", required=false, defaultValue= "false") boolean horrorSort,
                           @RequestParam(name="strategySort", required=false, defaultValue= "false") boolean strategySort,
                           Model model) throws IOException, InterruptedException, ParseException, JSONException {
 
             String KEY = "42771867b81b456496770e0c1c15d4f2";
-            String url = "https://api.rawg.io/api/games?key=" + KEY + "&search=" + search;
+            String url = "https://api.rawg.io/api/games?key=" + KEY + "&GetGameDetailsBySlug?slug=rpg";
 
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(url))
@@ -61,29 +62,10 @@ public class Reccomendations {
             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
             System.out.println(response.body());
 
-            JSONArray gameList = new JSONObject(response.body()).getJSONArray("results");
-            System.out.println(gameList.length());
+            // JSONArray gameList = new JSONObject(response.body()).getJSONArray("results");
+            // System.out.println(gameList.length());
 
-            ArrayList<String[]> retArr = new ArrayList<>();
-
-            for (int i = 0; i < gameList.length(); i++) {
-                retArr.add(
-                        new String[] {gameList.getJSONObject(i).getString("name"), gameList.getJSONObject(i).getString("id"),gameList.getJSONObject(i).getString("background_image"), gameList.getJSONObject(i).getString("rating"), gameList.getJSONObject(i).getString("playtime")}
-                );
-            }
-
-            if (strategySort){
-                retArr.sort(new StrategyComparator());
-            }
-            if (rpgSort){
-                retArr.sort(new PlayTimeComparator());
-            }
-            if (horrorSort){
-                retArr.sort(new PlayTimeComparator());
-            }
-
-            model.addAttribute("gameList", retArr);
-            return "recommendations"; // returns HTML VIEW (greeting)
+            return "/recommendations"; // returns HTML VIEW (greeting)
     }
 }
 
