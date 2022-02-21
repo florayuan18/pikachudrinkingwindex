cb79e00f6f7f84d7aa7d52c952755123c4e5ce77
crystalwidjaja <pansypig@gmail.com>
2022-02-20 16:30:02 -0800

    quiz polymorphism done

--- src/main/java/com/example/sping_portfolio/controllers/quiz/Sage.java
+++
@@ -1,8 +0,0 @@
-package com.example.sping_portfolio.controllers.quiz;
-
-public class Sage extends VideoGameSounds{
-    @Override
-    public void Sound() {
-        super.Sound();
-    }
-}
--- src/main/java/com/example/sping_portfolio/controllers/quiz/VideoGameSounds.java
+++
@@ -1,7 +0,0 @@
-package com.example.sping_portfolio.controllers.quiz;
-
-public class VideoGameSounds {
-    public void Sound(){
-        System.out.println("Who says this voice line?");
-    }
-}
